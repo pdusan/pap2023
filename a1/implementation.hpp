8fa95f7a8c1b1f1ad3c0caf43c44e98f8b106970
Dusan Popovic <dusanp96@gmail.com>
2023-11-19 02:54:53 +0100

    Revert "first try"
    This reverts commit b8aa7b33cb54fbdac5cedcaf41f7bd0eae0f80f2.

--- a1/implementation.hpp
+++ a1/implementation.hpp
@@ -2,30 +2,25 @@
 #include <omp.h>
 #include "helpers.hpp"
 
-unsigned long SequenceInfo::gpsa_sequential(float **S, float **SUB, std::unordered_map<char, int> &cmap)
-{
-	unsigned long visited = 0;
-	gap_penalty = SUB[0][cmap['*']]; // min score
+unsigned long SequenceInfo::gpsa_sequential(float** S, float** SUB, std::unordered_map<char, int>& cmap) {
+    unsigned long visited = 0;
+    gap_penalty = SUB[0][cmap['*']]; // min score
 
 	// Boundary
-	for (unsigned int i = 1; i < rows; i++)
-	{
-		S[i][0] = i * gap_penalty;
+    for (unsigned int i = 1; i < rows; i++) {
+        S[i][0] = i * gap_penalty;
 		visited++;
 	}
 
-	for (unsigned int j = 0; j < cols; j++)
-	{
-		S[0][j] = j * gap_penalty;
+    for (unsigned int j = 0; j < cols; j++) {
+        S[0][j] = j * gap_penalty;
 		visited++;
 	}
 
 	// Main part
-	for (unsigned int i = 1; i < rows; i++)
-	{
-		for (unsigned int j = 1; j < cols; j++)
-		{
-			float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
+	for (unsigned int i = 1; i < rows; i++) {
+		for (unsigned int j = 1; j < cols; j++) {
+			float match = S[i - 1][j - 1] + SUB[ cmap.at(X[i - 1]) ][ cmap.at(Y[j-1]) ];
 			float del = S[i - 1][j] + gap_penalty;
 			float insert = S[i][j - 1] + gap_penalty;
 			S[i][j] = std::max({match, del, insert});
@@ -34,14 +29,13 @@ unsigned long SequenceInfo::gpsa_sequential(float **S, float **SUB, std::unorder
 		}
 	}
 
-	return visited;
+    return visited;
 }
 
-unsigned long SequenceInfo::gpsa_taskloop(float **S, float **SUB, std::unordered_map<char, int> cmap, int grain_size = 1)
-{
-	// Your work goes here, paralellize the code below using OpenMP taskloop
-	unsigned long visited = 0;
-	gap_penalty = SUB[0][cmap['*']]; // min score
+unsigned long SequenceInfo::gpsa_taskloop(float** S, float** SUB, std::unordered_map<char, int> cmap, int grain_size=1) {
+	// Your work goes here, paralellize the code below using OpenMP taskloop 
+    unsigned long visited = 0;
+    gap_penalty = SUB[0][cmap['*']]; // min score
 
 	// Boundary
 	#pragma omp parallel
@@ -84,31 +78,27 @@ unsigned long SequenceInfo::gpsa_taskloop(float **S, float **SUB, std::unordered
 	return visited;
 }
 
-unsigned long SequenceInfo::gpsa_tasks(float **S, float **SUB, std::unordered_map<char, int> cmap, int grain_size = 1)
-{
-	// Your work goes here, paralellize the code below using OpenMP tasks
-	unsigned long visited = 0;
-	gap_penalty = SUB[0][cmap['*']]; // min score
+
+unsigned long SequenceInfo::gpsa_tasks(float** S, float** SUB, std::unordered_map<char, int> cmap, int grain_size=1) {
+	// Your work goes here, paralellize the code below using OpenMP tasks 
+    unsigned long visited = 0;
+    gap_penalty = SUB[0][cmap['*']]; // min score
 
 	// Boundary
-	for (unsigned int i = 1; i < rows; i++)
-	{
-		S[i][0] = i * gap_penalty;
+    for (unsigned int i = 1; i < rows; i++) {
+        S[i][0] = i * gap_penalty;
 		visited++;
 	}
 
-	for (unsigned int j = 0; j < cols; j++)
-	{
-		S[0][j] = j * gap_penalty;
+    for (unsigned int j = 0; j < cols; j++) {
+        S[0][j] = j * gap_penalty;
 		visited++;
 	}
 
 	// Main part
-	for (unsigned int i = 1; i < rows; i++)
-	{
-		for (unsigned int j = 1; j < cols; j++)
-		{
-			float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
+	for (unsigned int i = 1; i < rows; i++) {
+		for (unsigned int j = 1; j < cols; j++) {
+			float match = S[i - 1][j - 1] + SUB[ cmap.at(X[i - 1]) ][ cmap.at(Y[j-1]) ];
 			float del = S[i - 1][j] + gap_penalty;
 			float insert = S[i][j - 1] + gap_penalty;
 			S[i][j] = std::max({match, del, insert});
@@ -116,5 +106,5 @@ unsigned long SequenceInfo::gpsa_tasks(float **S, float **SUB, std::unordered_ma
 			visited++;
 		}
 	}
-	return visited;
+    return visited;
 }
