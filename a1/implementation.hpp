761b0ccadb247e3d4b23cbe252d51c5f6037f946
Dusan Popovic <dusanp96@gmail.com>
2023-11-19 04:46:08 +0100

    reverting back

--- a1/implementation.hpp
+++ a1/implementation.hpp
@@ -64,64 +64,24 @@ unsigned long SequenceInfo::gpsa_taskloop(float **S, float **SUB, std::unordered
 		}
 	}
 	
-	// Main part
-	// #pragma omp parallel
-	// {
-	// 	#pragma omp single
-	// 	#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) shared(S, SUB, cmap)
-	// 	for (unsigned int i = 1; i < rows; i++)
-	// 	{
-	// 		for (unsigned int j = 1; j < cols; j++)
-	// 		{
-	// 			float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
-	// 			float del = S[i - 1][j] + gap_penalty;
-	// 			float insert = S[i][j - 1] + gap_penalty;
-	// 			#pragma omp critical
-	// 			S[i][j] = std::max({match, del, insert});
-		
-	// 			visited++;
-	// 		}
-	// 	}
-	// }
-
-
-
-	// INTERESTING TEST HERE
-
-	#pragma omp parallel 
+	Main part
+	#pragma omp parallel
 	{
-		float **localS;
-
 		#pragma omp single
+		#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) shared(S, SUB, cmap)
+		for (unsigned int i = 1; i < rows; i++)
 		{
-			localS = new float*[omp_get_num_threads()];
-			for (unsigned int i = 0; i < omp_get_num_threads(); ++i)
-				localS[i] = new float[cols];
+			for (unsigned int j = 1; j < cols; j++)
+			{
+				float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
+				float del = S[i - 1][j] + gap_penalty;
+				float insert = S[i][j - 1] + gap_penalty;
+				#pragma omp critical
+				S[i][j] = std::max({match, del, insert});
+		
+				visited++;
+			}
 		}
-
-		int threadId = omp_get_thread_num();
-		#pragma omp tasklopp reduction(+ : visited) shared(S, localS, gap_penalty) firstprvate(rows, cols, SUB, cmap, X, Y)
-		for (unsigned int i = 1; i < rows; i++) {
-            for (unsigned int j = 1; j < cols; j++) {
-                float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
-                float del = S[i - 1][j] + gap_penalty;
-                float insert = S[i][j - 1] + gap_penalty;
-                localS[threadId][j] = std::max({match, del, insert});
-                visited++;
-            }
-        }
-
-		#pragma omp single
-		{
-			 // Combine the local copies into the global matrix
-                for (unsigned int i = 1; i < rows; i++) {
-                    for (unsigned int j = 1; j < cols; j++) {
-                        S[i][j] = localS[0][j];
-                    }
-                }
-                delete[] localS;
-		}
-
 	}
 
 	return visited;
