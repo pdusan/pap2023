2a322fd310dd1b78fc5e661cc17c10c0c445e90f
Dusan Popovic <dusanp96@gmail.com>
2023-11-19 03:38:16 +0100

    revert to normal

--- a1/implementation.hpp
+++ a1/implementation.hpp
@@ -65,47 +65,22 @@ unsigned long SequenceInfo::gpsa_taskloop(float **S, float **SUB, std::unordered
 	}
 	
 	// Main part
-	// #pragma omp parallel
-	// {
-	// 	#pragma omp single
-	// 	#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) firstprivate(SUB, cmap, X, Y) shared(S, gap_penalty)
-	// 	for (unsigned int i = 1; i < rows; i++)
-	// 	{
-	// 		for (unsigned int j = 1; j < cols; j++)
-	// 		{
-	// 			float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
-	// 			float del = S[i - 1][j] + gap_penalty;
-	// 			float insert = S[i][j - 1] + gap_penalty;
-	// 			S[i][j] = std::max({match, del, insert});
+	#pragma omp parallel
+	{
+		#pragma omp single
+		#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) firstprivate(SUB, cmap, X, Y) shared(S, gap_penalty)
+		for (unsigned int i = 1; i < rows; i++)
+		{
+			for (unsigned int j = 1; j < cols; j++)
+			{
+				float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
+				float del = S[i - 1][j] + gap_penalty;
+				float insert = S[i][j - 1] + gap_penalty;
+				S[i][j] = std::max({match, del, insert});
 		
-	// 			visited++;
-	// 		}
-	// 	}
-
-	 #pragma omp parallel
-        {
-            #pragma omp single
-            {
-                for (int d = 1; d < rows + cols - 1; d++) {
-                    int start_row = std::max(1, d - (int)cols + 1);
-                    int end_row = std::min(d, (int)rows - 1);
-
-                    #pragma omp taskloop firstprivate(start_row, end_row, SUB, cmap, X, Y) reduction(+:visited) shared(S, gap_penalty)
-                    for (int i = start_row; i <= end_row; i++) {
-                        int j = d - i;
-                        float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
-                        float del = S[i - 1][j] + gap_penalty;
-                        float insert = S[i][j - 1] + gap_penalty;
-
-                        S[i][j] = std::max({match, del, insert});
-
-                        visited++;
-                    }
-                }
-            }
-
-
-
+				visited++;
+			}
+		}
 	}
 
 	return visited;
