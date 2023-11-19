13683ba275fd71b52224273af3ea431009581ead
Dusan Popovic <dusanp96@gmail.com>
2023-11-19 03:23:20 +0100

    keep main part sequential

--- a1/implementation.hpp
+++ a1/implementation.hpp
@@ -65,10 +65,10 @@ unsigned long SequenceInfo::gpsa_taskloop(float **S, float **SUB, std::unordered
 	}
 	
 	// Main part
-	#pragma omp parallel
-	{
-		#pragma omp single
-		#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) firstprivate(SUB, cmap, X, Y) shared(S, gap_penalty)
+	// #pragma omp parallel
+	// {
+	// 	#pragma omp single
+	// 	#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) firstprivate(SUB, cmap, X, Y) shared(S, gap_penalty)
 		for (unsigned int i = 1; i < rows; i++)
 		{
 			for (unsigned int j = 1; j < cols; j++)
@@ -81,7 +81,7 @@ unsigned long SequenceInfo::gpsa_taskloop(float **S, float **SUB, std::unordered
 				visited++;
 			}
 		}
-	}
+	// }
 
 	return visited;
 }
