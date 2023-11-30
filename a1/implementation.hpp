ba51da31becaeff67c74984778ffd239b11a758f
Dusan Popovic <dusanp96@gmail.com>
2023-11-30 17:28:14 +0100

    test

--- a1/implementation.hpp
+++ a1/implementation.hpp
@@ -65,24 +65,48 @@ unsigned long SequenceInfo::gpsa_taskloop(float **S, float **SUB, std::unordered
 	}
 	
 	#pragma omp parallel
+	#pragma omp single
 	{
-		#pragma omp single
-		for (int d = 1; d < rows+cols-1; d+=grain_size)
-		{
-			#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) shared(S, cmap) firstprivate(gap_penalty,SUB, d)
-			for (unsigned int i = std::max(1, d-cols+1); i < std::min(d,rows-1)+1; i++)
-			{
-				unsigned int j = d - i;
-				if (j>0) {
-					float match = S[i - 1][j - 1] + SUB[cmap.at(X[i - 1])][cmap.at(Y[j - 1])];
-					float del = S[i - 1][j] + gap_penalty;
-					float insert = S[i][j - 1] + gap_penalty;
-					S[i][j] = std::max({match, del, insert});
 
-					visited++;
-				}
-			}
-		}
+		for (int D = 0; D < rows + cols - 1 - grain_size; D+=grain_size) {
+
+    	    int steps = (int)D / grain_size;
+
+    	    // std::cout<<"steps: " << steps << std::endl;
+			#pragma omp taskloop grainsize(grain_size) shared(S)
+    	    for (int p = std::max((steps - grain_size) * grain_size, 0); p <= grain_size * steps; p+=grain_size) {
+    	        if (p < rows-1){
+    	            int q = std::abs(p - grain_size * steps);
+    	            if (q < rows-1) {
+
+    	                // std::cout << "p: " << p << " q: " << q << std::endl;
+						#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) shared(S)
+    	                for (int d = 0; d < 2 * grain_size - 1;d++) {
+    	                    // std::cout<<std::endl << "d: " << d << std::endl;
+    	                    for (int i = std::max(d - grain_size, 0); i <= d; i++)
+    	                    {
+    	                        int j = d - i;
+
+    	                        // std::cout << "i: " << i << " j: " << j << std::endl;
+	
+    	                        if (i < grain_size && j < grain_size && i >= 0 && j >= 0) {
+    	                            int m = i + p +1;
+    	                            int n = j + q +1;
+	
+    	                            // std::cout << "m: " << m << " n: " << n << std::endl;
+    	                            float match = S[n - 1][m - 1] + SUB[cmap.at(X[n - 1])][cmap.at(Y[m - 1])];
+									float del = S[n - 1][m] + gap_penalty;
+									float insert = S[n][m - 1] + gap_penalty;
+									S[n][m] = std::max({match, del, insert});
+
+						  			visited++;
+    	                        }
+    	                    }
+    	                }
+    	            }
+    	        }
+    	    }
+    	}
 	}
 
 	return visited;
--- a1/test.cpp
+++ a1/test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-const int SIZE = 12;
-const int BLOCK_SIZE = 4;
+const int SIZE = 7;
+const int BLOCK_SIZE = 1;
 
 int main() {
     int matrix[SIZE][SIZE];
@@ -19,21 +19,40 @@ int main() {
     std::cout << std::endl
               << "Matrix in antidiagonal pattern:" << std::endl;
 
-    // Traverse the matrix in antidiagonal pattern
-    for (int sum = 0; sum <= 2 * (SIZE - 1); sum+=BLOCK_SIZE) {
-        int steps = (int)sum / BLOCK_SIZE;
+    for (int D = 0; D < 2 * SIZE - 1 - BLOCK_SIZE; D+=BLOCK_SIZE) {
 
-        for (int d = steps; d < 2 * BLOCK_SIZE - 1; d++) {
-            for (int i = 0; i <= d; ++i)
-            {
-                int j = d - i;
-                if (i < SIZE && j < SIZE)
-                {
-                    std::cout << matrix[i][j] << " ";
+        int steps = (int)D / BLOCK_SIZE;
+
+        // std::cout<<"steps: " << steps << std::endl;
+
+        for (int p = std::max((steps - BLOCK_SIZE) * BLOCK_SIZE, 0); p <= BLOCK_SIZE * steps; p+=BLOCK_SIZE) {
+            if (p < SIZE-1){
+                int q = std::abs(p - BLOCK_SIZE * steps);
+                if (q < SIZE-1) {
+
+                    // std::cout << "p: " << p << " q: " << q << std::endl;
+    
+                    for (int d = 0; d < 2 * BLOCK_SIZE - 1;d++) {
+                        // std::cout<<std::endl << "d: " << d << std::endl;
+                        for (int i = std::max(d - BLOCK_SIZE, 0); i <= d; i++)
+                        {
+                            int j = d - i;
+
+                            // std::cout << "i: " << i << " j: " << j << std::endl;
+    
+                            if (i < BLOCK_SIZE && j < BLOCK_SIZE && i >= 0 && j >= 0) {
+                                int m = i + p +1;
+                                int n = j + q +1;
+    
+                                // std::cout << "m: " << m << " n: " << n << std::endl;
+                                std::cout<< ' ' << matrix[n][m];
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 
-    return 0;
+        return 0;
 }
