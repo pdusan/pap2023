c4287356ebdd14566b885090410f95525387b3d5
Dusan Popovic <a01651258@alma.par.univie.ac.at>
2023-12-04 01:26:23 +0100

    submission

--- a1/implementation.hpp
+++ a1/implementation.hpp
@@ -42,136 +42,147 @@ unsigned long SequenceInfo::gpsa_taskloop(float **S, float **SUB, std::unordered
 	// Your work goes here, paralellize the code below using OpenMP taskloop
 	unsigned long visited = 0;
 	gap_penalty = SUB[0][cmap['*']]; // min score
-	// Boundary
-	#pragma omp parallel
-	#pragma omp single
+
+	int tile_size = 129;
+// Boundary
+#pragma omp parallel
+#pragma omp single
 	{
-		#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) shared(S, rows) firstprivate(gap_penalty)
+#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) shared(S, rows) firstprivate(gap_penalty)
 		for (unsigned int i = 1; i < rows; i++)
 		{
 			S[i][0] = i * gap_penalty;
 			visited++;
 		}
 
-		#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) shared(S, cols) firstprivate(gap_penalty)
+#pragma omp taskloop grainsize(grain_size) reduction(+ : visited) shared(S, cols) firstprivate(gap_penalty)
 		for (unsigned int j = 0; j < cols; j++)
 		{
 			S[0][j] = j * gap_penalty;
 			visited++;
 		}
 	}
-	
-	#pragma omp parallel
-	#pragma omp single
+
+#pragma omp parallel
+#pragma omp single
 	{
-		for (int D = 0; D < rows + cols - 1 - grain_size; D+=grain_size) {
-        	int steps = (int)D / grain_size;
-		
-			#pragma omp taskloop grainsize(grain_size) reduction(+: visited) shared(S, SUB)
-    	    for (int p = std::max((steps - grain_size) * grain_size, 0); p <= grain_size * steps; p+=grain_size) {
-    	        if (p < rows-1){
-    	            int q = std::abs(p - grain_size * steps);
-    	            if (q < rows-1) {
+		for (int D = 0; D < rows + cols - 1 - tile_size; D += tile_size)
+		{
+			int steps = (int)D / tile_size;
+#pragma omp taskloop reduction(+ : visited) grainsize(grain_size)
+			for (int p = std::max((steps - tile_size) * tile_size, 0); p <= tile_size * steps; p += tile_size)
+			{
+				if (p < rows - 1)
+				{
+					int q = std::abs(p - tile_size * steps);
+					if (q < rows - 1)
+					{
 
-						#pragma omp taskloop grainsize(grain_size) reduction(+: visited) shared(S, SUB)
-    	                for (int d = 0; d < 2 * grain_size - 1;d++) {
-    	                    for (int i = std::max(d - grain_size, 0); i <= d; i++)
-    	                    {
-    	                        int j = d - i;
+						for (int d = 0; d < 2 * tile_size - 1; d++)
+						{
+#pragma omp taskloop reduction(+ : visited) grainsize(grain_size)
+							for (int i = std::max(d - tile_size, 0); i <= d; i++)
+							{
+								int j = d - i;
 
-	
-    	                        if (i < grain_size && j < grain_size && i >= 0 && j >= 0) {
-    	                            int m = i + p +1;
-    	                            int n = j + q +1;
-	
-    	                            float match = S[n - 1][m - 1] + SUB[cmap.at(X[n - 1])][cmap.at(Y[m - 1])];
+								if (i < tile_size && j < tile_size && i >= 0 && j >= 0)
+								{
+									int m = i + p + 1;
+									int n = j + q + 1;
+
+									float match = S[n - 1][m - 1] + SUB[cmap.at(X[n - 1])][cmap.at(Y[m - 1])];
 									float del = S[n - 1][m] + gap_penalty;
 									float insert = S[n][m - 1] + gap_penalty;
 									S[n][m] = std::max({match, del, insert});
 
-						  			visited++;
-    	                        }
-    	                    }
-    	                }
-    	            }
-    	        }
-    	    }
-    	}
+									visited++;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 	return visited;
 }
 
-unsigned long SequenceInfo::gpsa_tasks(float **S, float **SUB, std::unordered_map<char, int> cmap, int grain_size = 1)
+unsigned long SequenceInfo::gpsa_tasks(float **S, float **SUB, std::unordered_map<char, int> cmap, int tile_size = 1)
 {
 	// Your work goes here, paralellize the code below using OpenMP tasks
 	unsigned long visited = 0;
 	gap_penalty = SUB[0][cmap['*']]; // min score
 
-	// Boundary
-	#pragma omp parallel
-	#pragma omp single
+// Boundary
+#pragma omp parallel
+#pragma omp single
 	{
-		#pragma omp taskgroup task_reduction(+: visited)
+#pragma omp taskgroup task_reduction(+ : visited)
 		for (unsigned int i = 1; i < rows; i++)
 		{
-			#pragma omp task in_reduction(+:visited)
-			{	
+#pragma omp task in_reduction(+ : visited)
+			{
 				S[i][0] = i * gap_penalty;
 				visited++;
 			}
 		}
 
-		#pragma omp taskgroup task_reduction(+: visited)
+#pragma omp taskgroup task_reduction(+ : visited)
 		for (unsigned int j = 0; j < cols; j++)
-		{	
-			#pragma omp task in_reduction(+:visited)
-			{	
+		{
+#pragma omp task in_reduction(+ : visited)
+			{
 				S[0][j] = j * gap_penalty;
 				visited++;
 			}
 		}
 	}
 
-	#pragma omp parallel
-	#pragma omp single
+#pragma omp parallel
+#pragma omp single
 	{
-		for (int D = 0; D < rows + cols - 1 - grain_size; D+=grain_size) {
+		for (int D = 0; D < rows + cols - 1 - tile_size; D += tile_size)
+		{
 
-    	    int steps = (int)D / grain_size;
+			int steps = (int)D / tile_size;
+#pragma omp taskgroup task_reduction(+ : visited)
+			for (int p = std::max((steps - tile_size) * tile_size, 0); p <= tile_size * steps; p += tile_size)
+			{
+				if (p < rows - 1)
+				{
+					int q = std::abs(p - tile_size * steps);
+					if (q < rows - 1)
+					{
 
-    	    for (int p = std::max((steps - grain_size) * grain_size, 0); p <= grain_size * steps; p+=grain_size) {
-    	        if (p < rows-1){
-    	            int q = std::abs(p - grain_size * steps);
-    	            if (q < rows-1) {
-						
-						#pragma omp taskgroup
-    	                for (int d = 0; d < 2 * grain_size - 1;d++) {
-    	                    for (int i = std::max(d - grain_size, 0); i <= d; i++)
-    	                    {
-    	                        int j = d - i;
+#pragma omp taskgroup task_reduction(+ : visited)
+						for (int d = 0; d < 2 * tile_size - 1; d++)
+						{
+							for (int i = std::max(d - tile_size, 0); i <= d; i++)
+							{
+								int j = d - i;
 
-	
-    	                        if (i < grain_size && j < grain_size && i >= 0 && j >= 0) {
-    	                            int m = i + p +1;
-    	                            int n = j + q +1;
+								if (i < tile_size && j < tile_size && i >= 0 && j >= 0)
+								{
+									int m = i + p + 1;
+									int n = j + q + 1;
 
-									#pragma omp task 
+#pragma omp task in_reduction(+ : visited)
 									{
-    	                            	float match = S[n - 1][m - 1] + SUB[cmap.at(X[n - 1])][cmap.at(Y[m - 1])];
+										float match = S[n - 1][m - 1] + SUB[cmap.at(X[n - 1])][cmap.at(Y[m - 1])];
 										float del = S[n - 1][m] + gap_penalty;
 										float insert = S[n][m - 1] + gap_penalty;
 										S[n][m] = std::max({match, del, insert});
 
-						  				visited++;
+										visited++;
 									}
-    	                        }
-    	                    }
-    	                }
-    	            }
-    	        }
-    	    }
-    	}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 	return visited;
